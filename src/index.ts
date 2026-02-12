4a4793403c09164741d393ff075a127f19a2f98b
Moreno Feltscher <moreno@smartive.ch>
2026-02-12 16:21:17 +0100

    fix: default exports are breaking Next.js client components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export * from './cache-tags';
-export * as postgres from './cache-tags';
-export * as redis from './cache-tags-redis';
 export * from './classnames';
 export * from './links';
 export * from './types';
