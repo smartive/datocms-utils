719861b8dacbc309f9a6c46d4504c8a5a53bec79
Moreno Feltscher <moreno@smartive.ch>
2026-02-13 01:06:31 +0100

    feat!: refactor cache implementation to support multiple backends and switch to ESM modules
    Please note: cache tag functionality has been rewritten and is now available as a separate export: @smartive/datocms-utils/cache. The new implementation supports multiple backends, including Redis and Postgres, and is designed to be more flexible and extensible. Please refer to the updated documentation for details on how to use the new cache functionality.

---
+++ src/cache/index.ts
@@ -0,0 +1,4 @@
+export * from './store/postgres.js';
+export * from './store/redis.js';
+export * from './types.js';
+export * from './utils.js';
---
+++ src/cache/store/redis.ts
@@ -0,0 +1,61 @@
+import { Redis } from 'ioredis';
+import { type CacheTag, type CacheTagsStore } from '../types.js';
+
+export const createRedisCacheTagsStore = ({ url, keyPrefix = '' }: { url: string; keyPrefix?: string }): CacheTagsStore => {
+  const redis = new Redis(url, {
+    maxRetriesPerRequest: 3,
+    lazyConnect: true,
+  });
+
+  const storeQueryCacheTags = async (queryId: string, cacheTags: CacheTag[]) => {
+    if (!cacheTags?.length) {
+      return;
+    }
+
+    const pipeline = redis.pipeline();
+
+    for (const tag of cacheTags) {
+      pipeline.sadd(`${keyPrefix}${tag}`, queryId);
+    }
+
+    await pipeline.exec();
+  };
+
+  const queriesReferencingCacheTags = async (cacheTags: CacheTag[]) => {
+    if (!cacheTags?.length) {
+      return [];
+    }
+
+    const keys = cacheTags.map((tag) => `${keyPrefix}${tag}`);
+
+    return redis.sunion(...keys);
+  };
+
+  const deleteCacheTags = async (cacheTags: CacheTag[]) => {
+    if (!cacheTags?.length) {
+      return 0;
+    }
+
+    const keys = cacheTags.map((tag) => `${keyPrefix}${tag}`);
+
+    return redis.del(...keys);
+  };
+
+  const truncateCacheTags = async () => {
+    const pattern = `${keyPrefix}*`;
+    const keys = await redis.keys(pattern);
+
+    if (keys.length === 0) {
+      return 0;
+    }
+
+    return await redis.del(...keys);
+  };
+
+  return {
+    storeQueryCacheTags,
+    queriesReferencingCacheTags,
+    deleteCacheTags,
+    truncateCacheTags,
+  };
+};
